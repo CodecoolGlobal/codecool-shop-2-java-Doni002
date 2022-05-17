6ee70dcdbfa65c6d156fcbd11e2faa8eec2be231
doni002 <alpardonat02@gmail.com>
2022-05-17 15:05:32 +0200

    Add all cars to main screen at ProductController.java

--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.codecool.shop.service;
 
+import com.codecool.shop.config.DataBaseInitializer;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductService{
